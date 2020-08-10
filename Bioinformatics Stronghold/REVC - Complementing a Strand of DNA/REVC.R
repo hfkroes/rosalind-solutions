99d0900a00545f0b9d6588d55b842e5d3391558d
Hector Fugihara Kroes <hector.kroes@outlook.com>
2020-08-10 09:57:49 -0300

    Add files via upload

--- Bioinformatics Stronghold/REVC - Complementing a Strand of DNA/REVC.R
+++ Bioinformatics Stronghold/REVC - Complementing a Strand of DNA/REVC.R
@@ -6,11 +6,11 @@ library(clipr)
 #Setting working directory
 setwd("D:/_Hector/R") 
 
-#Reading sequence txt file
+#Reading the sequence txt file
 file = readtext(paste0(getwd(), "/rosalind_revc.txt"))
 DNAsequence = DNAString(x = file$text[[1]])
 
-#Transcribing sequence
+#Reverse complementing the sequence
 RevCompl = reverseComplement(DNAsequence)
 
 #Printing accordingly
