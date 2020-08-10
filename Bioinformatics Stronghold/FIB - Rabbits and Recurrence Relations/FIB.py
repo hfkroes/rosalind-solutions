dec9635618ba75187b4284e9d5e3234a5d87c967
Hector Fugihara Kroes <hector.kroes@outlook.com>
2020-08-10 10:46:45 -0300

    Add files via upload

--- Bioinformatics Stronghold/FIB - Rabbits and Recurrence Relations/FIB.py
+++ Bioinformatics Stronghold/FIB - Rabbits and Recurrence Relations/FIB.py
@@ -1,7 +1,25 @@
+#Importing libraries
+import pyperclip
+
+#Reading variables txt file
+file = open('rosalind_fib.txt', 'r')
+
+variables = file.read().split(' ')
+n = int(variables[0])
+k = int(variables[1])
+
+#Defining function
 def rabbit(n, k):
 	if n==1 or n==2:
 		return 1
 	else:
 		return (rabbit(n-1, k)+(k*(rabbit(n-2, k))))
 
-print (rabbit(5, 3))
+#Calculating rabbit number
+rabbit_number = rabbit(n, k)
+
+#Printing accordingly
+print(rabbit_number)
+
+#Copying to clipboard for easier submition
+pyperclip.copy(rabbit_number)
