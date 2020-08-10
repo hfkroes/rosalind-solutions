ea115f98c7a64dcf2faa6484ca2219759aa197e9
Hector Fugihara Kroes <hector.kroes@outlook.com>
2020-08-10 09:51:26 -0300

    Add files via upload

--- Bioinformatics Stronghold/RNA - Transcribing DNA into RNA/RNA.R
+++ Bioinformatics Stronghold/RNA - Transcribing DNA into RNA/RNA.R
@@ -1,12 +1,13 @@
 #Importing libraries
 library(readtext)
 library(Biostrings)
+library(clipr)
 
 #Setting working directory
 setwd("D:/_Hector/R") 
 
 #Reading sequence txt file
-file = readtext(paste0(getwd(), "/rosalind_dna.txt"))
+file = readtext(paste0(getwd(), "/rosalind_rna.txt"))
 DNAsequence = DNAString(x = file$text[[1]])
 
 #Transcribing sequence
@@ -14,3 +15,6 @@ RNAsequence = RNAString(x = DNAsequence)
 
 #Printing accordingly
 print(RNAsequence)
+
+#Copy to clipboard for easy submition
+write_clip(as.character(RNAsequence))
