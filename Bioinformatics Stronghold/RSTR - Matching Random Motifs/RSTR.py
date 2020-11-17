5d10dbabbe659930cdbd8c898707b44d7316e118
Hector Fugihara Kroes <hector.kroes@fm.usp.br>
2020-11-17 08:58:35 -0300

    Add files via upload

---
+++ Bioinformatics Stronghold/RSTR - Matching Random Motifs/RSTR.py
@@ -0,0 +1,37 @@
+#Importing libraries
+import pyperclip, itertools
+
+#Reading variables txt file
+file = open('rosalind_rstr.txt', 'r')
+input_file = file.read().split('\n')
+
+variables = input_file[0].split(' ')
+string = input_file[1]
+
+n = int(variables[0])
+x = float(variables[1])
+
+#Building function
+def prob(n, gc, seq):
+    percent = 1
+    prob_gc = gc/2
+    prob_at = (1-gc)/2
+    
+    for i in seq:
+        if i == 'G' or i == 'C':
+            percent *= prob_gc
+        elif i == 'A' or i == 'T':
+            percent *= prob_at
+
+    percent = 1 - (1 - percent) ** n
+    
+    return percent
+
+#Calculating probability
+res = prob(n, x, string)
+
+#Printing result
+print("%.3f" % res)
+
+#Copying to clipboard for easier submition
+pyperclip.copy("%.3f" % res)
